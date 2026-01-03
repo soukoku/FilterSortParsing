47ba1f86fd28d62db5b73641ccb903e66a17b446
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2026-01-03 09:49:38 -0500

    Add project files.

---
+++ FilterSortParsing/QueryableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace FilterSortParsing;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> ApplyFilter<T>(this IQueryable<T> source, string filter)
+    {
+        if (string.IsNullOrWhiteSpace(filter))
+        {
+            return source;
+        }
+        return source;
+    }
+
+
+    public static IQueryable<T> ApplyOrderBy<T>(this IQueryable<T> source, string orderBy)
+    {
+        if (string.IsNullOrWhiteSpace(orderBy))
+        {
+            return source;
+        }
+        return source;
+    }
+}
